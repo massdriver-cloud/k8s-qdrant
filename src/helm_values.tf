0d4b49eb7ed3696086d67026f5fb69a760733d2a
mclacore <michael@massdriver.cloud>
2023-11-22 11:30:35 -0700

    Final changes and operator guide

--- src/helm_values.tf
+++ src/helm_values.tf
@@ -2,16 +2,19 @@ locals {
   helm_values = {
     nameOverride = var.md_metadata.name_prefix
     podLabels    = var.md_metadata.default_tags
-    replicaCount = var.replica_configuration.replicas
+    replicaCount = var.database.replica_configuration.replicas
     resources = {
       requests = {
-        cpu    = "${var.instance_configuration.cpu_limit}"
-        memory = "${var.instance_configuration.memory_limit}Gi"
+        cpu    = var.database.instance_configuration.cpu_limit / 2
+        memory = ceil(var.database.instance_configuration.memory_limit / 2)
       }
       limits = {
-        cpu    = "${var.instance_configuration.cpu_limit}"
-        memory = "${var.instance_configuration.memory_limit}Gi"
+        cpu    = var.database.instance_configuration.cpu_limit
+        memory = var.database.instance_configuration.memory_limit
       }
+    }
+    persistence = {
+      size = "${var.database.instance_configuration.storage_size}Gi"
     }
     apiKey = random_password.api-key.result
   }
